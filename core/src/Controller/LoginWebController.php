45cd755c6d469afaf2bfff546d2d488a452de6bd
maoxtrem <maoxtrem@gmail.com>
2026-01-13 10:58:32 -0500

    ajustes

--- core/src/Controller/LoginWebController.php
+++ core/src/Controller/LoginWebController.php
@@ -4,39 +4,20 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
 
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 
 class LoginWebController extends AbstractController
 {
-    #[Route('/login', methods: ['POST'])]
-    public function login(Request $request, UserProviderInterface $provider): JsonResponse
+    #[Route('/login', name: 'login', methods: ['POST'])]
+    public function login(): Response
     {
-        $data = json_decode($request->getContent(), true);
-
-        $user = $provider->loadUserByIdentifier($data['username']);
-
-        if ($data['password'] !== 'admin') {
-            return new JsonResponse(['error' => 'invalid credentials'], 401);
-        }
-
-        $token = new UsernamePasswordToken(
-            $user,
-            'main',
-            $user->getRoles()
-        );
-
-        $this->container->get('security.token_storage')->setToken($token);
-        $request->getSession()->set('_security_main', serialize($token));
-
-        return new JsonResponse(['status' => 'logged_in']);
+       return new Response('', 204);
+        throw new \LogicException('This should never be reached!');
     }
-
 
     #[Route('/me', methods: ['GET'])]
     public function me(): JsonResponse
